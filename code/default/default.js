7b2ddda1241c1636cde549dae5d7af7cf91f9679
Kolja <lasar@rasal.de>
2022-09-19 08:03:47 +0200

    ADD better ifframe, icons, TRY editor theme

--- code/default/default.js
+++ code/default/default.js
@@ -1,7 +1,7 @@
-console.log('default');
+console.info('Welcome to HCJ');
 
 document.getElementById('test').addEventListener('click', () => {
-  console.log('test clicked')
+  console.log('Test button clicked')
 })
 
 let li = document.querySelectorAll('li');
