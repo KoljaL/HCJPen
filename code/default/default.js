d33b43741554c0b1a5156c781e42c8d6f39be467
Kolja <lasar@rasal.de>
2022-09-21 12:04:22 +0200

    reduced to one file per pen

--- code/default/default.js
+++
@@ -1,8 +0,0 @@
-console.info('Welcome to HCJ');
-
-document.getElementById('test').addEventListener('click', () => {
-  console.log('Test button clicked')
-})
-
-let li = document.querySelectorAll('li');
-console.log(li)
